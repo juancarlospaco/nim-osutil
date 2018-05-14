18e00b50bde92350f6450a3e91682e0bc9da53a0
Juan Carlos <juan@thirstie.com>
2018-05-14 09:08:42 -0300

    Add Clipboard functionalities.

--- src/osutil.nim
+++ src/osutil.nim
@@ -18,15 +18,26 @@ when defined linux:
     return
 
   proc set_process_name*(name: string): string =
+    ## Set this process name from argument string using LibC.prctl().
     prctl(name=cstring(name))
 
 
-  if is_main_module:
-    # If you dont set the process name, usually its always "nim" or "main".
-    echo "Open your system monitor and check the process name of this."
-    echo set_process_name("MY_PROCESS_NAME")
-    # sleep 99999
+# proc clipboard_copy*(content: string): tuple[output: TaintedString, exitCode: int] =
+#   ## Clipboard copy functionality.
+#   when defined linux:
+#     return execCmdEx("xclip -selection clipboard")
+#   elif defined macosx:
+#     return execCmdEx("pbcopy")
+
+
+proc clipboard_paste*(): tuple[output: TaintedString, exitCode: int] =
+  ## Clipboard paste functionality.
+  when defined linux:
+    return execCmdEx("xclip -selection clipboard -o")
+  elif defined macosx:
+    return execCmdEx("pbpaste")
 
 
 if is_main_module:
   echo set_display_off()
+  echo set_process_name("MY_PROCESS_NAME")
